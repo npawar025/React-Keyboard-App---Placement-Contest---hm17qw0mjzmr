e310b588d197e9db48f298ddf5813fd10e8acfb0
Nilesh Pawar <119921719+npawar025@users.noreply.github.com>
2023-05-28 15:36:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,36 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
+  const [text, setText] = useState("");
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.content);
+      });
+  }, []);
+
+  const handleClick = (e) => {
+    let s = new String(text);
+    s += e.target.name;
+    setText(s);
+  };
 
   return (
     <div className="keyboard">
-      <div className="preview"></div>
+      {text != "forty two" && <div className="preview">{text}</div>}
+      {text == "forty two" && <div className="quote">{quote}</div>}
+
       <div>
         {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
+          <button
+            name={key}
+            onClick={handleClick}
+            key={key}
+            id={key === " " ? `key-space` : `key-${key}`}
+          >
             {key === " " ? "Space" : key.toUpperCase()}
           </button>
         ))}
